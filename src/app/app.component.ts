4868c9cdd30c3075ed567fd4d00f3af5543f8e17
Alex Caskey <42579463+AlexCaskey1017@users.noreply.github.com>
2021-10-24 19:35:06 -0500

    uploading my changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+const SESSION_COOKIE_MINUTES = 5;
+const SESSION_COOKIE_IDENTIFIER = 'name=session';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SERS';
+  isLogin = true;
+  usernameValue = '';
+  passwordValue = '';
+
+  ngOnInit(): void {
+    const hasSession = document.cookie
+    .split('; ')
+    .find((piece: string) => piece === SESSION_COOKIE_IDENTIFIER);
+
+    if (hasSession) {
+      this.isLogin = false;
+    }
+  }
+
+  logout(): void {
+    this.isLogin = true;
+    this.usernameValue = '';
+    this.passwordValue = '';
+
+    // Remove session cookie
+    document.cookie = `${SESSION_COOKIE_IDENTIFIER}; max-age=0`;
+  }
+
+  login(): void {
+    if (this.usernameValue && this.passwordValue) {
+      this._loginSuccess();
+    }
+  }
+
+  private _loginSuccess(): void {
+    this.isLogin = false;
+    const seconds = SESSION_COOKIE_MINUTES * 60; 
+    document.cookie = `${SESSION_COOKIE_IDENTIFIER}; max-age=${seconds}`;
+  }
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { MapComponent } from './map/map.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ReportsComponent } from './reports/reports.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketsComponent,
+    MapComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    RouterModule.forRoot(
+      [
+        { path: 'tickets', component: TicketsComponent },
+        { path: 'map', component: MapComponent },
+        { path: 'reports', component: ReportsComponent }
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
